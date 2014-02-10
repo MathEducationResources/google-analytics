c8db03b2a4c454bd85f3cfd028a98cdd18aa1bdf
BernhardKonrad <bernhard.konrad@gmail.com>
2014-02-10 11:18:26 -0800

    Several adjustments to deal with larger data set

--- MERhelpers.py
+++ MERhelpers.py
@@ -116,6 +116,8 @@ def visit_duration_dist(page_info, course, include_subpages=True):
                 front_and_subpages_dist[exam] += page_info[course][exam][question]['visit_duration']
         except ValueError:
             continue
+        except KeyError:
+        	continue
     if not include_subpages:
         return front_page_dist
     else:
@@ -123,14 +125,18 @@ def visit_duration_dist(page_info, course, include_subpages=True):
 
 def plot_visit_duration_dist(page_info, course, style='pie', include_subpages=True):
     dict_to_plot = visit_duration_dist(page_info, course, include_subpages)
-    y = np.zeros(len(dict_to_plot.keys()))
+    y = []
     my_labels = []
     for num,key in enumerate(sorted(dict_to_plot.keys())):
-        y[num] = dict_to_plot[key]
+    	# disregard entries below 1%
+    	if float(dict_to_plot[key])/sum(dict_to_plot.values()) < 0.01:
+    		continue
+        y.append(dict_to_plot[key])
         if 'pie' == style:
             my_labels.append(key.replace('_','') + ' [' + str(int(y[num]/60/60)) + 'h ' + str(int(100*round(float(dict_to_plot[key])/sum(dict_to_plot.values()),2))) + '%]')
         elif 'bar' == style:
-            my_labels.append(key.replace('December_','Dec').replace('April_','Apr') + '\n' + str(int(100*round(float(dict_to_plot[key])/sum(dict_to_plot.values()),2))) + '%')
+            my_labels.append(key.replace('December_','Dec').replace('April_','Apr').replace('20','') + '\n' + str(int(100*round(float(dict_to_plot[key])/sum(dict_to_plot.values()),2))) + '%')
+    y = np.asarray(y)
     if 'pie' == style:
         plt.pie(y, labels = my_labels, startangle = 90)
     elif 'bar' == style:
@@ -186,11 +192,15 @@ def fraction_course_exam_question_by_key(mydict,mykey):
 def plot_question_dist(mydict, course, exam, mykey, plot_type = 'bar'):
 	question_keys = sorted(list_questions(mydict, course, exam))
 	if len(question_keys) > 1:
-		y = np.zeros(len(question_keys))
+		y = []
 		my_labels = []
-		for num, question in enumerate(question_keys):
+		num = 0
+		for question in question_keys:
 			value = mydict[course][exam][question][mykey]
-			y[num] = value
+			#disregard pages with less than 5 unique pageviews. High chance these are typos
+			if 5 > mydict[course][exam][question]['unique_pageviews']:
+				continue
+			y.append(float(value))
 			label_question = question.replace('_','').replace('Question','Q')
 			if 'pageviews' == mykey:
 				label_data = str(value)
@@ -212,6 +222,8 @@ def plot_question_dist(mydict, course, exam, mykey, plot_type = 'bar'):
 				my_labels.append(label_question)
 			elif 'pie' == plot_type:
 				my_labels.append(label_question + ' ' + label_data)
+			num += 1
+		y = np.asarray(y)
 		if 'bar' == plot_type:
 			plt.bar(range(len(y)),y,align='center',color=create_RGB_list(len(y)))
 			plt.gca().set_xticks(range(len(y)))
@@ -225,34 +237,35 @@ def plot_question_dist(mydict, course, exam, mykey, plot_type = 'bar'):
 		plt.title(plot_title)
 
 
-def get_avg_exam_time(mydict):
-    for course in list_courses(mydict):
-        avg_exam_time[course] = {}
-        for exam in list_exams(mydict, course):
-            avg_exam_time[course][exam] = 0
-            questions = list_questions(mydict, course, exam)
-            for question in questions:
-                avg_exam_time[course][exam] += mydict[course][exam][question]['avg_time']
-            try:
-                avg_exam_time[course][exam] /= len(questions)
-            except ZeroDivisionError:
-                del avg_exam_time[course][exam]
-        for courses in avg_exam_time.keys():
-            if not avg_exam_time[courses]:
-                del avg_exam_time[courses]
-    return avg_exam_time
+def get_total_exam_time(mydict):
+	total_exam_time = {}
+	for course in list_courses(mydict):
+		total_exam_time[course] = {}
+		for exam in list_exams(mydict, course):
+			total_exam_time[course][exam] = 0
+			for question in list_questions(mydict, course, exam):
+				total_exam_time[course][exam] += mydict[course][exam][question]['avg_time']
+		for courses in total_exam_time.keys():
+			if not total_exam_time[courses]:
+				del total_exam_time[courses]
+	return total_exam_time
 
-def print_avg_exam_time(avg_exam_time):
-    y = np.zeros(len(avg_exam_time[course]))
+
+def print_total_exam_time(total_exam_time, course):
+    y = []
     my_label = []
-    for num,exam in enumerate(avg_exam_time[course]):
-        y[num] = avg_exam_time[course][exam]
+    for exam in total_exam_time[course]:
+    	#remove exams where total time was less than threshold (in s)
+    	if 10*60 > total_exam_time[course][exam]:
+    		continue
+        y.append(float(total_exam_time[course][exam])/60)
         my_label.append(exam.replace('_',' '))
+    y = np.asarray(y)
     plt.bar(range(len(y)), y, align='center', color=create_RGB_list(len(y)))
     plt.gca().set_xticks(range(len(y)))
     plt.gca().set_xticklabels(my_label)
     plt.xticks(rotation='vertical')
-    plt.ylabel('Average time per question')
+    plt.ylabel('Total time per exam in m, using average for each question')
     plt.title(course)
 
 
@@ -260,10 +273,10 @@ def plot_total_clicks_time_series(filename):
 	num_clicks = []
 	date_list = []
 	for line in open(filename,'r'):
-		line = line.strip()
+		line = line.strip().replace('"','')
 		line = line.split(',')
-		date_list.append(line[0])
-		num_clicks.append(int(line[1]))
+		date_list.append(line[0].strip())
+		num_clicks.append(int(line[1].strip()))
 
 	x_axis=[]
 	x_axis.append(date_list.index('1/1/12'))
@@ -272,83 +285,84 @@ def plot_total_clicks_time_series(filename):
 	x_axis.append(date_list.index('1/1/13'))
 	x_axis.append(date_list.index('5/1/13'))
 	x_axis.append(date_list.index('9/1/13'))
+	x_axis.append(date_list.index('1/1/14'))
 
 	myarray = np.asarray(num_clicks)
 	plt.plot(myarray)
 	plt.ylabel('Number of clicks')
 	plt.title('Total number of clicks: ' + str(np.sum(myarray)))
 	plt.gca().set_xticks(x_axis)
-	plt.gca().set_xticklabels(['Jan 12','May 12','Sep 12','Jan 13','May 13','Sep 13'])
+	plt.gca().set_xticklabels(['Jan 12','May 12','Sep 12','Jan 13','May 13','Sep 13','Jan 14'])
 
 	## Exams  ##
 	# DEC 2013
 	index = date_list.index('12/10/13')
-	plt.plot([index,index],[0,70000],'k-')
+	plt.plot([index,index],[0,70000],'k:')
 	plt.text(index-35, 65000, 'Math 100')
 
 	index = date_list.index('12/11/13')
-	plt.plot([index,index],[0,70000],'k-')
+	plt.plot([index,index],[0,70000],'k:')
 	plt.text(index+5, 50000, 'Math 102')
 
 	index = date_list.index('12/11/13')
-	plt.plot([index,index],[0,70000],'k-')
+	plt.plot([index,index],[0,70000],'k:')
 	plt.text(index+5, 35000, 'Math 104')
 
 	index = date_list.index('12/11/13')
-	plt.plot([index,index],[0,70000],'k-')
+	plt.plot([index,index],[0,70000],'k:')
 	plt.text(index+5, 20000, 'Math 110')
 
 	# APR 2013
 	index = date_list.index('4/15/13')
-	plt.plot([index,index],[0,70000],'k-')
+	plt.plot([index,index],[0,70000],'k:')
 	plt.text(index-35, 40000, 'Math 152')
 
 	index = date_list.index('4/19/13')
-	plt.plot([index,index],[0,70000],'g-')
+	plt.plot([index,index],[0,70000],'g:')
 	plt.text(index-38, 65000, 'Math 103', color='g')
 
 	index = date_list.index('4/22/13')
-	plt.plot([index,index],[0,70000],'k-')
+	plt.plot([index,index],[0,70000],'k:')
 	plt.text(index+5, 50000, 'Math 101')
 
 	index = date_list.index('4/22/13')
-	plt.plot([index,index],[0,70000],'k-')
+	plt.plot([index,index],[0,70000],'k:')
 	plt.text(index+5, 35000, 'Math 105')
 
 	index = date_list.index('4/22/13')
-	plt.plot([index,index],[0,70000],'k-')
+	plt.plot([index,index],[0,70000],'k:')
 	plt.text(index+5, 20000, 'Math 110')
 
 	# DEC 2012
 	index = date_list.index('12/14/12')
-	plt.plot([index,index],[0,70000],'k-')
+	plt.plot([index,index],[0,70000],'k:')
 	plt.text(index+5, 65000, 'Math 102')
 
 	index = date_list.index('12/5/12')
-	plt.plot([index,index],[0,70000],'k-')
+	plt.plot([index,index],[0,70000],'k:')
 	plt.text(index-35, 50000, 'Math 100')
 
 	index = date_list.index('12/5/12')
-	plt.plot([index,index],[0,70000],'k-')
+	plt.plot([index,index],[0,70000],'k:')
 	plt.text(index-35, 35000, 'Math 104')
 
 	index = date_list.index('12/5/12')
-	plt.plot([index,index],[0,70000],'k-')
+	plt.plot([index,index],[0,70000],'k:')
 	plt.text(index-35, 20000, 'Math 110')
 
 	# APR 2012
 	index = date_list.index('4/16/12')
-	plt.plot([index,index],[0,70000],'k-')
+	plt.plot([index,index],[0,70000],'k:')
 	plt.text(index+5, 65000, 'Math 152')
 
 	index = date_list.index('4/11/12')
-	plt.plot([index,index],[0,70000],'k-')
+	plt.plot([index,index],[0,70000],'k:')
 	plt.text(index-35, 50000, 'Math 101')
 
 	index = date_list.index('4/11/12')
-	plt.plot([index,index],[0,70000],'k-')
+	plt.plot([index,index],[0,70000],'k:')
 	plt.text(index-35, 35000, 'Math 103')
 
 	index = date_list.index('4/11/12')
-	plt.plot([index,index],[0,70000],'k-')
+	plt.plot([index,index],[0,70000],'k:')
 	plt.text(index-35, 20000, 'Math 105')
